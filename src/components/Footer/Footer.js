1c58bab870055310807f1db40e5266bc5eb71f94
osiyo <merewall@gmail.com>
2021-09-18 04:04:01 -0400

    manifest

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     <footer className="flex-row px-1">
       <a href="https://github.com/merewall" target="_blank" rel="noreferrer" alt="GitHub profile link"><i className="fab fa-github" alt="GitHub logo"></i></a>
       <a href="https://www.linkedin.com/in/meredithwall/" target="_blank" rel="noreferrer" alt="LinkedIn link"><i className="fab fa-linkedin" alt="LinkedIn logo"></i></a>
-      <a href="https://www.instagram.com/merewall" target="_blank" rel="noreferrer"  alt="Instagram profile link"><i class="fab fa-instagram" alt="Instagram icon"></i></a>      
+      <a href="https://www.instagram.com/merewall" target="_blank" rel="noreferrer"  alt="Instagram profile link"><i className="fab fa-instagram" alt="Instagram icon"></i></a>      
     </footer>
   );
 }
