f6a9ae79d07677f03215d78600462caf0ea9d9d8
osiyo <merewall@gmail.com>
2021-09-18 03:35:44 -0400

    adding alt text for profile links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,9 @@ import './footer.css'
 export default function Footer() {
   return (
     <footer className="flex-row px-1">
-      <a href="https://github.com/merewall" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
-      <a href="https://www.linkedin.com/in/meredithwall/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
-      <a href="https://www.instagram.com/merewall" target="_blank" rel="noreferrer"><i class="fab fa-instagram"></i></a>      
+      <a href="https://github.com/merewall" target="_blank" rel="noreferrer" alt="GitHub profile link"><i className="fab fa-github" alt="GitHub logo"></i></a>
+      <a href="https://www.linkedin.com/in/meredithwall/" target="_blank" rel="noreferrer" alt="LinkedIn link"><i className="fab fa-linkedin" alt="LinkedIn logo"></i></a>
+      <a href="https://www.instagram.com/merewall" target="_blank" rel="noreferrer"  alt="Instagram profile link"><i class="fab fa-instagram" alt="Instagram icon"></i></a>      
     </footer>
   );
 }
